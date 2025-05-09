f66b237bfc61df5c021c30e2d7044773dce585b2
Ishan Kapoor <77106858+ishan-kapoor@users.noreply.github.com>
2025-05-09 09:37:04 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-vision-preview")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="meta-llama/llama-4-scout-17b-16e-instruct")
 
 
 if __name__ == "__main__":
